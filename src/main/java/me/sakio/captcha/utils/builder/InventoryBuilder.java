a8bb77bd26b3b340771359ba3e1401ba5d758b95
Sakio_ <65667910+Sakiio@users.noreply.github.com>
2021-05-26 17:40:17 +0200

    Delete src/main/java/me/sakio/captcha/utils/builder directory

--- src/main/java/me/sakio/captcha/utils/builder/InventoryBuilder.java
+++
@@ -1,8 +0,0 @@
-package me.sakio.captcha.utils.builder;
-
-public abstract class InventoryBuilder extends InventoryMaker implements InventoryLoader {
-
-    public InventoryBuilder(String title, int rows) {
-        super(title, rows);
-    }
-}
--- src/main/java/me/sakio/captcha/utils/builder/InventoryLoader.java
+++
@@ -1,6 +0,0 @@
-package me.sakio.captcha.utils.builder;
-
-public interface InventoryLoader {
-
-    void loadButtons();
-}
