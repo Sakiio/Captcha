b6838a13e6dd3312d2c25ceaa20e7d87bb5de893
Joseluu315 <65667910+Sakiio@users.noreply.github.com>
2022-04-17 13:59:53 +0200

    perf: add constructor

--- src/main/java/me/sakio/captcha/listener/CaptchaListener.java
+++ src/main/java/me/sakio/captcha/listener/CaptchaListener.java
@@ -1,7 +1,6 @@
 package me.sakio.captcha.listener;
 
 import me.sakio.captcha.CaptchaPlugin;
-import me.sakio.captcha.menu.CaptchaMenu;
 import me.sakio.captcha.utils.Color;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -11,7 +10,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * Created by DevSakio
@@ -21,28 +19,38 @@ import org.bukkit.event.player.PlayerJoinEvent;
  */
 public class CaptchaListener implements Listener {
 
+    private final CaptchaPlugin plugin;
+
+    public CaptchaListener(CaptchaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onCaptchaClose(InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+
         if (event.getInventory().getName().equals(ChatColor.RED + "Click the emerald block!")) {
-            Player player = (Player) event.getPlayer();
             if (!CaptchaPlugin.getPlayerArrayList().contains(player))
-                player.kickPlayer(Color.translate(CaptchaPlugin.getInstance().getConfig().getString("FAILED")));
+                player.kickPlayer(Color.translate(plugin.getConfig().getString("FAILED")));
         }
     }
 
     @EventHandler
     public void onCaptchaClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
+
         if (event.getInventory().getTitle().equals(ChatColor.RED + "Click the emerald block!")) {
             event.setCancelled(true);
+
             if (event.getCurrentItem() != null && !event.getCurrentItem().getType().equals(Material.EMERALD_BLOCK)) {
-                player.sendMessage(Color.translate(CaptchaPlugin.getInstance().getConfig().getString("FAILED")));
+                player.sendMessage(Color.translate(plugin.getConfig().getString("FAILED")));
                 player.playSound(player.getLocation(), Sound.VILLAGER_NO, 100, 100);
                 return;
             }
+
             CaptchaPlugin.getPlayerArrayList().add(player);
             player.closeInventory();
-            player.sendMessage(Color.translate(CaptchaPlugin.getInstance().getConfig().getString("PASSED")));
+            player.sendMessage(Color.translate(plugin.getConfig().getString("PASSED")));
             player.playSound(player.getLocation(), Sound.VILLAGER_YES, 100, 100);
         }
     }
